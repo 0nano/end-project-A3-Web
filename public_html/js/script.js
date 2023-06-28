95a7e6b0e515139663eb4485144837c0237f582b
aaannntttooo <107413153+aaannntttooo@users.noreply.github.com>
2023-06-28 16:57:40 +0200

    Carte

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,79 +1,46 @@
+for (i=0; i<2800; i++){
+    ajaxRequest('GET', 'api/index.php/accidents?offset='+i, total)
+}
+//ajaxRequest('GET', 'api/index.php/accidents?offset=1', blabla)
+//mettre le resultat de la requete dans une variable
+var data_lon = [];
+var data_lat=[];
+var data_ville=[];
+
+function total(data){
+        data_full = data_full.concat(data);
+  
+let data_lon = data_full.map(row => row['longitude']);
+let data_lat = data_full.map(row => row['latitude']);
+let data_ville = data_full.map(row => row['ville']);
 
 
-
-//FONCTION POUR L'AFFICHAGE DE LA CARTE
-
-d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2015_06_30_precipitation.csv', function(err, rows){
-      function unpack(rows, key) {
-          return rows.map(function(row) { return row[key]; });
-		}
-
-var data = [{
-        type: 'scattermapbox', text: unpack(rows, 'Globvalue'),
-        lon: unpack(rows, 'Lon'), lat: unpack(rows, 'Lat'),
-        marker: {color: 'fuchsia', size: 4}
-    }];
+var data = [
+	{
+		type: "scattermapbox",
+        Text: data_ville,
+		lon: data_lon,
+		lat: data_lat,
+		marker: { color: "fuchsia", size: 4 }
+	}
+];
 
 var layout = {
-	dragmode: 'zoom',
+	dragmode: "zoom",
 	mapbox: {
-		style: 'white-bg',
+		style: "white-bg",
 		layers: [
 			{
-            "below": 'traces',
-            "sourcetype": "raster",
-            "source": [
-                "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
-            ]
-        },
-			{
-             sourcetype: "raster",
-			 source: ["https://geo.weather.gc.ca/geomet/?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"]}],
-		below: 'traces',
-		center: {lat: 47, lon: 3}, zoom: 5},
-	margin: {r:0, t: 0, b: 0, l: 0}, 
-	showlegend: false};
+				sourcetype: "raster",
+				source: ["https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"],
+				below: "traces"
+			}
+		],
+		center: { lat: 47 , lon: 2.5},
+		zoom: 5
+	},
+	margin: { r: 0, t: 0, b: 0, l: 0 }
+};
 
-Plotly.newPlot('myDiv', data, layout);
-  });
-
-
-
-  
-
-
-//FONCTION POUR L'AFFICHAGE DE LA CARTE
-
-d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2015_06_30_precipitation.csv', function(err, rows){
-    function unpack(rows, key) {
-        return rows.map(function(row) { return row[key]; });
-      }
-
-var data = [{
-      type: 'scattermapbox', text: unpack(rows, 'Globvalue'),
-      lon: unpack(rows, 'Lon'), lat: unpack(rows, 'Lat'),
-      marker: {color: 'fuchsia', size: 4}
-  }];
-
-var layout = {
-  dragmode: 'zoom',
-  mapbox: {
-      style: 'white-bg',
-      layers: [
-          {
-          "below": 'traces',
-          "sourcetype": "raster",
-          "source": [
-              "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
-          ]
-      },
-          {
-           sourcetype: "raster",
-           source: ["https://geo.weather.gc.ca/geomet/?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"]}],
-      below: 'traces',
-      center: {lat: 47, lon: 3}, zoom: 5},
-  margin: {r:0, t: 0, b: 0, l: 0}, 
-  showlegend: false};
-
-Plotly.newPlot('myDivPredict', data, layout);
-});
+Plotly.newPlot("myDiv", data, layout);
+}
