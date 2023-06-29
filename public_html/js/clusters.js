9f6f1cf42478587f3cee920d231e70fcdf435702
Mart-1 <martin.lobel@isen-ouest.yncrea.fr>
2023-06-29 14:42:41 +0200

    lien ajax

--- public_html/js/clusters.js
+++ public_html/js/clusters.js
@@ -2,7 +2,7 @@ console.log("cluster");
 var lng = 25;
 var lat = 45;
 
-ajaxRequest('GET', 'index.html/api/index.php/clusters', displayClusters, `prediction&lat=${lat}&lng=${lng}`);
+ajaxRequest('GET', 'api/index.php/clusters', displayClusters, `prediction&lat=${lat}&lng=${lng}`);
 
 function displayClusters(clusters){
     console.log(clusters);
