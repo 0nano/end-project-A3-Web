ff5382bca4b9412dbe93d81e27e19c609b1ea138
Mart-1 <martin.lobel@isen-ouest.yncrea.fr>
2023-06-29 14:49:41 +0200

    recupère les variables du json

--- public_html/js/clusters.js
+++ public_html/js/clusters.js
@@ -1,9 +1,17 @@
-console.log("cluster");
 var lng = 25;
 var lat = 45;
 
 ajaxRequest('GET', 'api/index.php/clusters', displayClusters, `prediction&lat=${lat}&lng=${lng}`);
 
-function displayClusters(clusters){
-    console.log(clusters);
+function displayClusters(data){
+    var lat_cluster = data['clusters']['latitude du centroid'];
+    var lng_cluster = data['clusters']['longitude du centroid'];
+
+    var lat_acc = data['accident']['latitude de l\'accident'];
+    var lng_acc = data['accident']['longitude de l\'accident'];
+    console.log(lat_cluster);
+    console.log(lng_cluster);
+    console.log(lat_acc);
+    console.log(lng_acc);
+
 }
