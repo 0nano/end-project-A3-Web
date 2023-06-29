15e2dbff4a189d1e19afee33ebc6911acfbc1877
Mart-1 <martin.lobel@isen-ouest.yncrea.fr>
2023-06-29 14:50:52 +0200

    a

--- public_html/js/clusters.js
+++ public_html/js/clusters.js
@@ -4,6 +4,7 @@ var lat = 45;
 ajaxRequest('GET', 'api/index.php/clusters', displayClusters, `prediction&lat=${lat}&lng=${lng}`);
 
 function displayClusters(data){
+    console.log(data);
     var lat_cluster = data['clusters']['latitude du centroid'];
     var lng_cluster = data['clusters']['longitude du centroid'];
 
